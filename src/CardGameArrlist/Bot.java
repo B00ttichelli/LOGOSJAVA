32d0285a44ac61bc7428f0607e0f2990fe8915f9
B00ttichelli <donttrytofish@protonmail.com>
2021-07-20 10:32:56 +0300

    origin

--- src/CardGameArrlist/Bot.java
+++ src/CardGameArrlist/Bot.java
@@ -1,7 +1,9 @@
 package CardGameArrlist;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Bot extends Player{
 
@@ -9,7 +11,22 @@ public class Bot extends Player{
 
     @Override
     public Card Respond(ArrayList<Card> table) {
-        return null;
+        Card card = table.get(table.size()-1);
+
+        List<Card> bigestInHand = hand.stream().filter(
+                (a) -> a.getSuit().equals(card.getSuit())).filter(
+                (a) -> a.getRank().ordinal() > card.getRank().ordinal()).collect(Collectors.toList());
+        //добавляем козыри
+        bigestInHand.addAll(hand.stream().filter((a)->a.getSuit().equals(getTramp())).collect(Collectors.toList()));
+        if(bigestInHand.size() == 0){
+            return null;
+
+        } else {
+            int randNumber = random.nextInt(bigestInHand.size());
+            hand.remove(bigestInHand.get(randNumber));
+            return bigestInHand.get(randNumber);
+        }
+
     }
 
     @Override
