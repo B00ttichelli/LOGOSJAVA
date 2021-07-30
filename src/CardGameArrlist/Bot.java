03ed903255ce4b561c3490c254fdc07c0e75b9ac
B00ttichelli <donttrytofish@protonmail.com>
2021-07-31 01:54:14 +0300

    origin

--- src/CardGameArrlist/Bot.java
+++ src/CardGameArrlist/Bot.java
@@ -12,17 +12,25 @@ public class Bot extends Player{
     @Override
     public Card Respond(ArrayList<Card> table) {
         Card card = table.get(table.size()-1);
+        List<Card> bigestInHand;
+         if(!card.getSuit().equals(tramp)){
+              bigestInHand = hand.stream().filter(
+                     (a) -> a.getSuit().equals(card.getSuit())).filter(
+                     (a) -> a.getRank().ordinal() > card.getRank().ordinal()).collect(Collectors.toList());
+             //добавляем козыри
+             bigestInHand.addAll(hand.stream().filter((a)->a.getSuit().equals(getTramp())).collect(Collectors.toList()));
+         } else {
+             bigestInHand = hand.stream().filter(
+                     (a) -> a.getSuit().equals(card.getSuit())).filter(
+                     (a) -> a.getRank().ordinal() > card.getRank().ordinal()).collect(Collectors.toList());
+         }
 
-        List<Card> bigestInHand = hand.stream().filter(
-                (a) -> a.getSuit().equals(card.getSuit())).filter(
-                (a) -> a.getRank().ordinal() > card.getRank().ordinal()).collect(Collectors.toList());
-        //добавляем козыри
-        bigestInHand.addAll(hand.stream().filter((a)->a.getSuit().equals(getTramp())).collect(Collectors.toList()));
         if(bigestInHand.size() == 0){
-            System.out.println(this.name + " Cant beat your card !! And taking " + card.toString());
+            System.out.println(this.name + " Cant beat your card !! And taking " + card);
             return null;
 
         } else {
+            // моя логика рандом сцуко !!!
             int randNumber = random.nextInt(bigestInHand.size());
             Card responsiveCard  = bigestInHand.get(randNumber);
             hand.remove(responsiveCard);
